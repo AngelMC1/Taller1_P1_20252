8aa16a1d6b72f8f4de4f990750bdd5b0c146d711
AngelMC1 <miguecorrepie@hotmail.com>
2025-08-02 19:05:56 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home (request):
     #return HttpResponse('<h1> Welcome to Home Page </h1>')
     #return render(request, 'home.html')
-    return render (request, 'home.html', {'name':'Miguel Angel Correa'})
+    #return render (request, 'home.html', {'name':'Miguel Angel Correa'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        # lista únicamente la(s) película(s) cuyo título contiene el nombre buscado
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else: 
+        # lista todas las películas de la base de datos
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
    
 
 def About (request):
-    return HttpResponse('<h1> Welcome to About Page </h1>')
+    return render (request, 'about.html')
 # Create your views here.
